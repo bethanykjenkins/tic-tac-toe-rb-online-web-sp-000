6f2639afddcb0351371d8cb21b7af0eaebbf04cf
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2021-01-03 20:26:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,7 +64,7 @@ def turn(board)
   puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-      if valid_move?(board, index) 
+      if valid_move?(board, index) == true
         move(board, index, player)
            # move(board, index, player = "X")
       else
