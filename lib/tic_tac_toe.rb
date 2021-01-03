dce06f9cf75d4f068140203c659b49b87bf8bcf7
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2021-01-03 20:31:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,7 +21,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, player = "X")
+def move(board, index, player)
    board[index] = player
    return board
 end
@@ -66,7 +66,7 @@ def turn(board)
     input = gets.strip
     index = input_to_index(input)
       if valid_move?(board, index) == true
-        move(board, index)
+        move(board, index, player)
            # move(board, index, player = "X")
       else
         puts "Please enter 1-9:"
